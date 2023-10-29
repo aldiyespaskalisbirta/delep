1966eef4a936c4cce86e50c1dc28cdf995622bc6
aldiyespaskalisbirta <aldiyespaskalisbirta@gmail.com>
2023-10-29 11:12:31 +0700

    cappie

--- components/carousel.tsx
+++ components/carousel.tsx
@@ -9,10 +9,17 @@ function CarouselSlider() {
       nextArrow={({ handleNext }) => <div></div>}
       navigation={({ setActiveIndex, activeIndex, length }) => <div></div>}
       autoplay={true}
-      autoplayDelay={2000}
+      autoplayDelay={3000}
       loop={true}
+      transition={{ type: "tween" }}
       className="w-full"
     >
+      <Image
+        src="/assets/cappie1.jpeg"
+        alt="cappie"
+        fill
+        className="object-cover"
+      />
       <Image
         src="/assets/cappie2.jpeg"
         alt="cappie"
@@ -20,7 +27,25 @@ function CarouselSlider() {
         className="object-cover"
       />
       <Image
-        src="/assets/cappie1.jpeg"
+        src="/assets/cappie3.jpeg"
+        alt="cappie"
+        fill
+        className="object-cover"
+      />
+      <Image
+        src="/assets/cappie4.jpeg"
+        alt="cappie"
+        fill
+        className="object-cover"
+      />
+      <Image
+        src="/assets/cappie5.jpeg"
+        alt="cappie"
+        fill
+        className="object-cover"
+      />
+      <Image
+        src="/assets/cappie6.jpeg"
         alt="cappie"
         fill
         className="object-cover"
